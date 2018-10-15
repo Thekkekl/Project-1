c82472d0de0137c0eb184e0520809a847621b53c
Thekkekl <42657228+Thekkekl@users.noreply.github.com>
2018-10-15 14:22:49 -0600

    Add files via upload

---
+++ CalculateLetterGrade/CalculateLetterGrade/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CalculateLetterGrade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumberGrade = new System.Windows.Forms.Label();
+            this.lblLetterGrade = new System.Windows.Forms.Label();
+            this.txtNumberGrade = new System.Windows.Forms.TextBox();
+            this.txtLetterGrade = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumberGrade
+            // 
+            this.lblNumberGrade.AutoSize = true;
+            this.lblNumberGrade.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumberGrade.Location = new System.Drawing.Point(102, 60);
+            this.lblNumberGrade.Name = "lblNumberGrade";
+            this.lblNumberGrade.Size = new System.Drawing.Size(117, 17);
+            this.lblNumberGrade.TabIndex = 0;
+            this.lblNumberGrade.Text = "&Number grade:";
+            this.lblNumberGrade.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblLetterGrade
+            // 
+            this.lblLetterGrade.AutoSize = true;
+            this.lblLetterGrade.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetterGrade.Location = new System.Drawing.Point(102, 112);
+            this.lblLetterGrade.Name = "lblLetterGrade";
+            this.lblLetterGrade.Size = new System.Drawing.Size(102, 17);
+            this.lblLetterGrade.TabIndex = 4;
+            this.lblLetterGrade.Text = "Letter grade:";
+            this.lblLetterGrade.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtNumberGrade
+            // 
+            this.txtNumberGrade.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumberGrade.Location = new System.Drawing.Point(285, 60);
+            this.txtNumberGrade.Name = "txtNumberGrade";
+            this.txtNumberGrade.Size = new System.Drawing.Size(81, 25);
+            this.txtNumberGrade.TabIndex = 1;
+            // 
+            // txtLetterGrade
+            // 
+            this.txtLetterGrade.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLetterGrade.Location = new System.Drawing.Point(285, 112);
+            this.txtLetterGrade.Name = "txtLetterGrade";
+            this.txtLetterGrade.ReadOnly = true;
+            this.txtLetterGrade.Size = new System.Drawing.Size(81, 25);
+            this.txtLetterGrade.TabIndex = 5;
+            this.txtLetterGrade.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(114, 201);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 30);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(256, 201);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 30);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(493, 297);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtLetterGrade);
+            this.Controls.Add(this.txtNumberGrade);
+            this.Controls.Add(this.lblLetterGrade);
+            this.Controls.Add(this.lblNumberGrade);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculate Letter Grade";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumberGrade;
+        private System.Windows.Forms.Label lblLetterGrade;
+        private System.Windows.Forms.TextBox txtNumberGrade;
+        private System.Windows.Forms.TextBox txtLetterGrade;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
